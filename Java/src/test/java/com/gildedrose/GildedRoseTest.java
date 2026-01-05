5af0ccac3c21520b3ceaa160cb6ea0cb240ba7f9
TamirPalay <typalay@gmail.com>
2026-01-05 12:44:11 +0200

    test: sulfuras never changes

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -74,4 +74,19 @@ void qualityNeverAboveFifty() {
 
 }
 
+@Test
+void sulfuras_neverChanges() {
+    Item[] items = new Item[] {
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80)//only item with quality 80
+    };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(0, items[0].sellIn);
+    assertEquals(80, items[0].quality); //Sulfuras quality and sellIn never change
+}
+
+
+
 }
